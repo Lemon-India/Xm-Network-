282a46eec039436da2a0cfb61e1c4ac92b966446
xms/_ <110107438+xms7210-bot@users.noreply.github.com>
2024-03-21 00:09:00 +0530

    Add files via upload

--- project4/settings.py
+++ project4/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR , 'static'),
+STATIC_ROOT =os.apth.join(BASE_DIR, 'staticfiles_build','static')
